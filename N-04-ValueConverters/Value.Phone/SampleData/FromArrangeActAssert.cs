34a2afd7b8007cc8b5827203d282d874c06827f4
Stuart <lodge.stuart@gmail.com>
2013-07-11 07:36:02 +0100

    Design time data from AAA

---
+++ N-04-ValueConverters/Value.Phone/SampleData/FromArrangeActAssert.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Value.Phone.SampleData
+{
+    public class DesignTimeFirstViewModel : ViewModel
+    {
+        public DesignTimeFirstViewModel()
+        {
+            Foo = "I'm design time data";
+        }
+
+        private string _foo;
+        public string Foo
+        {
+            get { return _foo; }
+            set { _foo = value; OnPropertyChanged("Foo"); }
+        }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+ 
+}
