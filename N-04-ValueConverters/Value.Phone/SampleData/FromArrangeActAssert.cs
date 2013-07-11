f17bb60663d1a6e2eb8cbf62021d0048bc526a07
Stuart <lodge.stuart@gmail.com>
2013-07-11 07:52:15 +0100

    Even simpler design time model - INPC was not needed?

--- N-04-ValueConverters/Value.Phone/SampleData/FromArrangeActAssert.cs
+++ N-04-ValueConverters/Value.Phone/SampleData/FromArrangeActAssert.cs
@@ -6,34 +6,13 @@ using System.Text;
 
 namespace Value.Phone.SampleData
 {
-    public class DesignTimeFirstViewModel : ViewModel
+    public class DesignTimeFirstViewModel
     {
         public DesignTimeFirstViewModel()
         {
-            Foo = "I'm design time data";
+            Foo = "I'm simple design time data";
         }
 
-        private string _foo;
-        public string Foo
-        {
-            get { return _foo; }
-            set { _foo = value; OnPropertyChanged("Foo"); }
-        }
-    }
-
-    public class ViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this,
-                    new PropertyChangedEventArgs(propertyName));
-            }
-        }
-    }
-
- 
+        public string Foo { get; set; }
+    } 
 }
