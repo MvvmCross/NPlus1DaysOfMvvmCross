db89136798e2c3fd03ce09c8460ed815279a395d
Stuart <lodge.stuart@gmail.com>
2013-09-30 21:55:01 +0100

    N=2 updated

--- N-02-KittenView/KittenView.Touch/Bootstrap/FilePluginBootstrap.cs
+++ N-02-KittenView/KittenView.Touch/Bootstrap/FilePluginBootstrap.cs
@@ -1,6 +1,6 @@
 using Cirrious.CrossCore.Plugins;
 
-namespace Bootstrap
+namespace KittenView.Touch.Bootstrap
 {
     public class FilePluginBootstrap
         : MvxLoaderPluginBootstrapAction<Cirrious.MvvmCross.Plugins.File.PluginLoader, Cirrious.MvvmCross.Plugins.File.Touch.Plugin>
