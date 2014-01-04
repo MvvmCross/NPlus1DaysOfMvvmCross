9a207adc82658f7a9ee95c9f146d1faa82320ef2
Kerry Street <kerrycstreet@gmail.com>
2014-01-04 11:50:58 -0800

    N-11 KittenViewColctns 158,WP8,iOS7,3.1.1.-beta2

--- N-11-KittenView_Collections/KittenView.Droid/Bootstrap/DownloadCachePluginBootstrap.cs
+++ N-11-KittenView_Collections/KittenView.Droid/Bootstrap/DownloadCachePluginBootstrap.cs
@@ -1,6 +1,6 @@
 using Cirrious.CrossCore.Plugins;
 
-namespace Bootstrap
+namespace KittenView.Droid.Bootstrap
 {
     public class DownloadCachePluginBootstrap
         : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.DownloadCache.PluginLoader>
