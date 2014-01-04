03bfa598fecb32ed433f43759072b21113f7b40d
Kerry Street <kerrycstreet@gmail.com>
2014-01-04 09:43:41 -0800

    N-06 Books to 158,WP8,iOS7,3.1.1.-beta2

--- N-06-Books/Books.Droid/SplashScreen.cs
+++ N-06-Books/Books.Droid/SplashScreen.cs
@@ -1,9 +1,16 @@
 using Android.App;
+using Android.Content.PM;
 using Cirrious.MvvmCross.Droid.Views;
 
 namespace Books.Droid
 {
-    [Activity(Label = "Books.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(
+		Label = "Books.Droid"
+		, MainLauncher = true
+		, Icon = "@drawable/icon"
+		, Theme = "@style/Theme.Splash"
+		, NoHistory = true
+		, ScreenOrientation = ScreenOrientation.Portrait)]
     public class SplashScreen : MvxSplashScreenActivity
     {
         public SplashScreen()
