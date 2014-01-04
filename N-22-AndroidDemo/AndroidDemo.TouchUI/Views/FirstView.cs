4cd361eb30ff7e2c72efdc7dd69755bfc1c9ad3a
Kerry Street <kerrycstreet@gmail.com>
2014-01-04 14:55:02 -0800

    N-22 AndroidDemo 158,iOS7,3.1.1.-beta2 binary tst

--- N-22-AndroidDemo/AndroidDemo.TouchUI/Views/FirstView.cs
+++ N-22-AndroidDemo/AndroidDemo.TouchUI/Views/FirstView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Drawing;
 using MonoTouch.Foundation;
+using MonoTouch.ObjCRuntime;
 using MonoTouch.UIKit;
 using Cirrious.MvvmCross.Touch.Views;
 using Cirrious.MvvmCross.Binding.BindingContext;
@@ -17,6 +18,10 @@ namespace AndroidDemo.TouchUI
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
+
+            // ios7 layout
+            if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+                EdgesForExtendedLayout = UIRectEdge.None;
 
 			var set = this.CreateBindingSet<FirstView, FirstViewModel> ();
 			set.Bind (TheEdit2).To (vm => vm.Foo);
