6ca7b0a469253d191ebb9bf2864679bae5b6e1ff
Kerry Street <kerrycstreet@gmail.com>
2014-01-04 12:08:01 -0800

    N-14 CollectABull-Part3 to 158,WP8,3.1.1.-beta2

---
+++ N-14-CollectABull-Part3/CollectABull.Phone/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace CollectABull.Phone.Views
+{
+    public partial class FirstView : MvxPhonePage
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
