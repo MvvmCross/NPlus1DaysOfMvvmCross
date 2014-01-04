e8f29465e21c759498f4bc05f246187bb3ce2d08
Kerry Street <kerrycstreet@gmail.com>
2014-01-04 10:32:17 -0800

    N-09-Location & Msg to 158,WP8,iOS7,3.1.1.-beta2

--- N-09-Location And Message/Location.Touch/Views/FirstView.cs
+++ N-09-Location And Message/Location.Touch/Views/FirstView.cs
@@ -1,6 +1,7 @@
 using System.Drawing;
 using Cirrious.MvvmCross.Binding.BindingContext;
 using Cirrious.MvvmCross.Touch.Views;
+using MonoTouch.ObjCRuntime;
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 
@@ -13,6 +14,10 @@ namespace Location.Touch.Views
         {
             View = new UIView(){ BackgroundColor = UIColor.White};
             base.ViewDidLoad();
+
+            // ios7 layout
+            if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+                EdgesForExtendedLayout = UIRectEdge.None;
 
             var label = new UILabel(new RectangleF(10, 10, 300, 40));
             Add(label);
