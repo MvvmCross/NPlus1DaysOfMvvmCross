433c7c724e3f8f4c39e3c8d1fd603a3d0146a4ce
Kerry Street <kerrycstreet@gmail.com>
2014-01-05 13:21:47 -0800

    N-40 Maps -droid frag issue,158,iOS7,3.1.1.-beta2

--- N-40-Maps/Mappit.Touch/Views/FirstView.cs
+++ N-40-Maps/Mappit.Touch/Views/FirstView.cs
@@ -4,6 +4,7 @@ using Cirrious.MvvmCross.Touch.Views;
 using Mappit.Core.ViewModels;
 using MonoTouch.CoreLocation;
 using MonoTouch.MapKit;
+using MonoTouch.ObjCRuntime;
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 
@@ -19,6 +20,10 @@ namespace Mappit.Touch.Views
             View = mapView;
 
             base.ViewDidLoad();
+
+            // ios7 layout
+            if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+                EdgesForExtendedLayout = UIRectEdge.None;
 
             var firstViewModel = (FirstViewModel) ViewModel;
             var helenAnnotation = new ZombieAnnotation(firstViewModel.Helen);
