5d0c9ad905dc0e5fcf0243f9dbb591ee379451d4
Kerry Street <kerrycstreet@gmail.com>
2014-01-05 11:31:12 -0800

    N-38-Maps to 158,iOS7,3.1.1.-beta2

--- N-38-Maps/Mappit.Touch/Views/FifthView.cs
+++ N-38-Maps/Mappit.Touch/Views/FifthView.cs
@@ -5,6 +5,7 @@ using Cirrious.MvvmCross.Touch.Views;
 using Mappit.Core.ViewModels;
 using MonoTouch.CoreLocation;
 using MonoTouch.MapKit;
+using MonoTouch.ObjCRuntime;
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 
@@ -19,6 +20,10 @@ namespace Mappit.Touch.Views
             View = mapView;
 
             base.ViewDidLoad();
+
+            // ios7 layout
+            if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+                EdgesForExtendedLayout = UIRectEdge.None;
 
             var regionManager = new RegionManager(mapView);
             mapView.Delegate = regionManager;
