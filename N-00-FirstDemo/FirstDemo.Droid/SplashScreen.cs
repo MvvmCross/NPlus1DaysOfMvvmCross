ca1fd0e85c1535fa9a360a297f221d1c3d003ef2
Maurice Markvoort <347817@student.saxion.nl>
2017-02-14 22:44:26 +0100

    Update N + 1 days series N-00-FirstDemo

--- N-00-FirstDemo/FirstDemo.Droid/SplashScreen.cs
+++ N-00-FirstDemo/FirstDemo.Droid/SplashScreen.cs
@@ -1,9 +1,16 @@
 using Android.App;
-using Cirrious.MvvmCross.Droid.Views;
+using Android.Content.PM;
+using MvvmCross.Droid.Views;
 
 namespace FirstDemo.Droid
 {
-    [Activity(Label = "FirstDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(
+        Label = "FirstDemo.Droid"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , NoHistory = true
+        , ScreenOrientation = ScreenOrientation.Portrait)]
     public class SplashScreen : MvxSplashScreenActivity
     {
         public SplashScreen()
