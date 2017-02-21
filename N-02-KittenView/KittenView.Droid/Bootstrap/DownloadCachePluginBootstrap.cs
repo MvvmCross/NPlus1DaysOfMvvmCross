40a91d5ff42e063dff895a3ad4061f738c389344
Maurice Markvoort <m.markvoort@xablu.com>
2017-02-21 10:02:20 +0100

    Fixed android not downloading images. I was on a lower android API level.

---
+++ N-02-KittenView/KittenView.Droid/Bootstrap/DownloadCachePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace KittenView.Droid.Bootstrap
+{
+    public class DownloadCachePluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.DownloadCache.PluginLoader>
+    {
+    }
+}
